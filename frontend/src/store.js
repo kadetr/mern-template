b90612d7e51117ac978513f286fac3bff41cba90
kadetr <kemaldeniz.teket@gmail.com>
2021-03-11 12:10:36 +0300

    user actions, reducers, contants, store

---
+++ frontend/src/store.js
@@ -0,0 +1,49 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import {
+   userLoginReducer,
+   userRegisterReducer,
+   userDetailsReducer,
+   userUpdateReducer,
+   userListAdminReducer,
+   userDeleteAdminReducer,
+   userUpdateAdminReducer,
+} from "./reducers/userReducers";
+
+const reducer = combineReducers({
+   userLogin: userLoginReducer,
+   userRegister: userRegisterReducer,
+   userDetails: userDetailsReducer,
+   userUpdate: userUpdateReducer,
+   userListAdmin: userListAdminReducer,
+   userDeleteAdmin: userDeleteAdminReducer,
+   userUpdateAdmin: userUpdateAdminReducer,
+});
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+   ? JSON.parse(localStorage.getItem("userInfo"))
+   : null;
+
+const initialState = {
+   userLogin: { userInfo: userInfoFromStorage },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+   reducer,
+   initialState,
+   composeWithDevTools(applyMiddleware(...middleware))
+);
+
+const middleware = [thunk];
+
+const store = createStore(
+   reducer,
+   initialState,
+   composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
